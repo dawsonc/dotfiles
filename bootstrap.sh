6e868c94aed105b831e7dbfe58a385cdceb9317a
Charles Dawson <charles.dwsn@gmail.com>
2023-04-21 14:00:43 -0400

    Modify git clone instructions

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 # Install git and clone dotfiles repo
 sudo apt install -y git
 cd ~
-git clone https://github.com/dawsonc/bootstrap.git
+git clone git@github.com/dawsonc/bootstrap.git
 
 # Run the dotfiles installation and setup scripts
 cd ~/dotfiles
