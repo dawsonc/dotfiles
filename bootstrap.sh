934d6c118387cf6793dffbfb8963f7acb0eaebee
Charles Dawson <charles.dwsn@gmail.com>
2023-04-21 14:07:11 -0400

    Update scripts in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,5 @@ git clone https://github.com/dawsonc/bootstrap.git
 
 # Run the dotfiles installation and setup scripts
 cd ~/dotfiles
-./install.sh
-./setup_dotfiles.sh
+./scripts/install.sh
+./scripts/setup_dotfiles.sh
