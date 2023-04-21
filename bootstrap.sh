6dc11564b053b0ba1eaab3294de187906512cd6d
Charles Dawson <charles.dwsn@gmail.com>
2023-04-21 13:55:38 -0400

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update software
+sudo apt update && sudo apt upgrade -y
+
+# Install git and clone dotfiles repo
+sudo apt install -y git
+cd ~
+git clone https://github.com/dawsonc/bootstrap.git
+
+# Run the dotfiles installation and setup scripts
+cd ~/dotfiles
+./install.sh
+./setup_dotfiles.sh
---
+++ scripts/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install basic development tools
+sudo apt install -y \
+    git \
+    build-essential \
+    python3 \
+    python3-pip \
+    python3-venv
+
+# Install the GitHub CLI tool
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install -y gh
+
+# Install VS Code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+# Install neovim
+sudo apt install neovim
+
+# Install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
